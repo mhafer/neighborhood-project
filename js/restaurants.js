a5d79a225bc543965bbbc2e7d95ce338257aa0d5
msalema <missmichellesalema@gmail.com>
2017-04-06 20:28:53 -0400

    initial commit

---
+++ js/restaurants.js
@@ -0,0 +1,222 @@
+var restaurants = [
+	{
+		name:"Lee's Pint & Shell",
+		street:"2844 Hudson St",
+		city:"Baltimore, MD",
+		type: ["Pub", "Bar", "Seafood"],
+		comments:"Great Food",
+		url:"www.leespintandshell.com",
+		lat:39.282347,
+		lng:-76.575689
+	},
+	{
+		name:"Sip & Bite",
+		street:"2200 Boston St",
+		city:"Baltimore, MD",
+		type: ["Breakfast","American", "Greek", "Diner", "Mediterranean"],
+		comments:"Great Food",
+		url:"www.sipandbite.com",
+		lat:39.283959,
+		lng:-76.585578
+	},
+	{
+		name:"Fork & Wrench",
+		street:"2322 Boston St",
+		city:"Baltimore, MD",
+		type: ["New American", "Fine Dining", "Cocktails"],
+		comments:"Great Food",
+		url:"www.theforkandwrench.com",
+		lat: 39.282901,
+		lng: -76.583881
+	},
+	{
+		name:"Dangerously Delicious",
+		street:"2839 O'Donnell St",
+		city:"Baltimore, MD",
+		type: ["Bakery","Dessert"],
+		comments:"Great Food",
+		url:"dangerouspiesbalt.com",
+		lat: 39.279923,
+		lng: -76.575537
+	},
+	{
+		name:"DiPasquales",
+		street:"3700 Gough St",
+		city:"Baltimore, MD",
+		type: ["Italian", "Deli"],
+		comments:"Great Food",
+		url:"www.dipasquales.com",
+		lat: 39.288904,
+		lng: -76.566489
+	},
+	{
+		name:"The Boathouse Canton",
+		street:"2809 Boston St",
+		city:"Baltimore, MD",
+		type: ["New American","Seafood","Contemporary"],
+		comments:"Great Food",
+		url:"www.boathousecanton.com",
+		lat: 39.276836,
+		lng: -76.576378
+	},
+		{
+		name:"Cask & Grain",
+		street:"2823 O'Donnell St",
+		city:"Baltimore, MD",
+		type: ["New American","Cocktails","Fine Dining"],
+		comments:"Great Food",
+		url:"caskandgrainkitchen.com",
+		lat: 39.279865,
+		lng: -76.575957
+	},
+	{
+		name:"Woodberry Kitchen",
+		street:"2010 Clipper Park Rd",
+		city:"Baltimore, MD",
+		type: ["American","Fine Dining"],
+		comments:"Great Food",
+		url:"www.woodberrykitchen.com",
+		lat: 39.332001,
+		lng: -76.64568
+	},
+	{
+		name:"Ra Sushi",
+		street:"1390 Lancaster St",
+		city:"Baltimore, MD",
+		type: ["Sushi", "Asian"],
+		comments:"Great Food",
+		url:"www.rasushi.com",
+		lat: 39.282465,
+		lng: -76.597435
+	},
+	{
+		name:"Ekiben",
+		street:"1622 Eastern Ave",
+		city:"Baltimore, MD",
+		type: ["Asian"],
+		comments:"Great Food",
+		url:"ekibenbaltimore.com",
+		lat: 39.285737,
+		lng: -76.594554
+	},
+	{
+		name:"Southern Provisions",
+		street:"3000 O'Donnell S",
+		city:"Baltimore, MD",
+		type: ["Southern", "Bar","Pub"],
+		comments:"Great Food",
+		url:"southern-provisions.com",
+		lat: 39.28056,
+		lng: -76.573867
+	},
+	{
+		name:"Verde",
+		street:"641 S Montford Ave",
+		city:"Baltimore, MD",
+		type: ["Italian","Pizza"],
+		comments:"Great Food",
+		url:"www.verdepizza.com",
+		lat: 39.284113,
+		lng: -76.582571
+	},
+	{
+		name:"HomeSlyce Canton",
+		street:"900 S Kenwood Ave",
+		city:"Baltimore, MD",
+		type: ["Pizza","Bar","Pub"],
+		comments:"Great Food",
+		url:"www.homeslyce.com",
+		lat: 39.282027,
+		lng: -76.577158
+	},
+	{
+		name:"Smaltimore",
+		street:"2522 Fait Ave",
+		city:"Baltimore, MD",
+		type: ["Bar","Pub"],
+		comments:"Great Food",
+		url:"smaltimorebaltimore.com",
+		lat: 39.283255,
+		lng: -76.58041
+	},
+	{
+		name:"Hudson Street Stack house",
+		street:"2626 Hudson St",
+		city:"Baltimore, MD",
+		type: ["Bar","Pub"],
+		comments:"Great Food",
+		url:"www.hudsonstreetstackhouse.com",
+		lat: 39.282294,
+		lng: -76.578838
+	},
+	{
+		name:"Kooper's Tavern",
+		street:"1702 Thames St",
+		city:"Baltimore, MD",
+		type: ["Pub","Bar"],
+		comments:"Great Food",
+		url:"www.kooperstavern.com",
+		lat: 39.282027,
+		lng: -76.592431
+	},
+	{
+		name:"The Abbey Burger Bistro",
+		street:"811 S Broadway",
+		city:"Baltimore, MD",
+		type: ["Pub","Bar","Burgers"],
+		comments:"Great Food",
+		url:"www.abbeyburgerbistro.com",
+		lat: 39.282204,
+		lng: -76.592697
+	},
+	{
+		name:"Shiso Tavern",
+		street:"2933 O'Donnell St",
+		city:"Baltimore, MD",
+		type: ["Sushi","Asian","Noodles"],
+		comments:"Great Food",
+		url:"http://www.shisotavern.com",
+		lat: 39.279923,
+		lng: -76.574212
+	},
+	{
+		name:"Jasa Kabob",
+		street:"2917 O'Donnell St",
+		city:"Baltimore, MD",
+		type: ["Asian","Indian"],
+		comments:"Great Food",
+		url:"www.jasakabob.com",
+		lat: 39.279906,
+		lng: -76.574872
+	},
+	{
+		name:"Saigon Today",
+		street:"700 S Potomac St",
+		city:"Baltimore, MD",
+		type: ["Asian","Noodles"],
+		comments:"Great Food",
+		url:"saigontodaybmore.com",
+		lat: 39.284189,
+		lng: -76.574399
+	},
+	{
+		name:"Blue Moon Cafe",
+		street:"1621 Aliceanna St",
+		city:"Baltimore, MD",
+		type: ["Breakfast","American"],
+		comments:"Great Food",
+		url:"www.bluemoonbaltimore.com",
+		lat: 39.283394,
+		lng: -76.594241
+	},
+	{
+		name:"Iron Rooster",
+		street:"3721 Boston St",
+		city:"Baltimore, MD",
+		type: ["Southern","Breakfast","American"],
+		comments:"Great Food",
+		url:"www.ironroosterallday.com",
+		lat: 39.276453,
+		lng: -76.565328
+	}
+];
